d61fdb77871f1353fea78443ef2519898ac6a1a2
fundream <fanjingsyu@gmail.com>
2020-06-17 03:26:02 +0800

    0616 update

--- script.js
+++ script.js
@@ -18,8 +18,8 @@ var vm = new Vue({
         time_to: " - ",
         time_end: "23:00",
         //ticket type & price
-        type: "5 Days Ticket",
-        price: "2500",
+        type: "1 Days Ticket",
+        price: "800",
         //location
         site: "LINE CUBE SHIBUYA",
         adress: "東京都渋谷区宇田川町1−1",
@@ -31,9 +31,69 @@ var vm = new Vue({
         status: false,
         //check
         openE: false,
+        openT: true,
+        show_ed: false,
+      },
+      {
+        //tital
+        eventtital: "SETOUCHI 2019",
+        subtital: "TRIENNALE",
+        //cover image
+        imgurl: "https://miro.medium.com/max/5660/1*HoQhlB6qu_9nAx5vEhQaNw.png",
+        //person
+        person: "fun_dream.11",
+        //time
+        date: "2019-03-20",
+        time_start: "10:00",
+        time_to: " - ",
+        time_end: "19:30",
+        //ticket type & price
+        type: "3 Season Ticket",
+        price: "1300",
+        //location
+        site: "Islands in Seto Inland Sea",
+        adress: "日本 岡山市と香川縣",
+        mapurl: "https://goo.gl/maps/ip52TcJXehvPeUg37",
+        //tatus
+        status_un: "UNUSED",
+        status_ued: "USED",
+        color_st: "#5E3D50",
+        status: true,
+        //check
+        openE: false,
         openT: false,
         show_ed: false,
       },
+      {
+        //tital
+        eventtital: "EVENT'S TITAL",
+        subtital: "SUBTITAL",
+        //cover image
+        imgurl: "",
+        //person
+        person: "Owner's ID",
+        //time
+        date: "",
+        time_start: "00:00",
+        time_to: " - ",
+        time_end: "01:00",
+        //ticket type & price
+        type: "Type of the ticket",
+        price: "0",
+        //location
+        site: "Name of the event's place",
+        adress: "Adress of the event's place",
+        mapurl: "",
+        //tatus
+        status_un: "UNUSED",
+        status_ued: "USED",
+        color_st: "#30527c",
+        status: false,
+        //check
+        openE: false,
+        openT: false,
+        show_ed: false,
+      }
       
     ],
     editors: [
