99c0e0e545676bd43e7dd2888b4a153ff45900bc
fundream <fanjingsyu@gmail.com>
2020-08-07 21:33:29 +0800

    [0807] v5 upload

--- src/script.js
+++ src/script.js
@@ -1,221 +1,236 @@
 var vm = new Vue({
   el: "#app",
   data: {
-    delMode: false,
+    //Mode_mode#Del_.btn_mode.delete_mode
+    deleteModeOnOff: false,
     events: [
-      //mutek
       {
-        //tital
-        eventtital: "MUTEK JP",
-        subtital: "EDITION4",
+        //Tickets_Ticket_ticket_Cover_cover
+        //Ticket & Editor's openning
+        cover_infoOpenClose: true,
+        editorOpenClose: false,
+        
+        //Tickets_Ticket_ticket_Cover
         //cover image
-        imgurl: "https://i.ytimg.com/vi/FZMXidFmtpw/maxresdefault.jpg",
+        image_url: "https://i.ytimg.com/vi/FZMXidFmtpw/maxresdefault.jpg",
+        
+        //Tickets_Ticket_ticket_info
+        //titel 
+        name_title: "MUTEK JP",
+        name_subtitle: "EDITION4",
         //person
-        person: "_fun.dream00",
+        content_ans_id: "_fun.dream00",
+        //date
+        content_ans_date_start: "2019-12-15",
+        content_ans_date_to: " - ",
+        content_ans_date_end: "2019-12-15",
         //time
-        date: "2019-12-15",
-        time_start: "19:00",
-        time_to: " - ",
-        time_end: "23:00",
-        //ticket type & price
-        type: "1 Days Ticket",
-        price: "800",
-        //location
-        site: "LINE CUBE SHIBUYA",
-        adress: "東京都渋谷区宇田川町1−1",
-        mapurl: "https://goo.gl/maps/JnXDN6azu7jPaV9x6",
-        //tatus
-        status_un: "UNUSED",
-        status_ued: "USED",
-        color_st: "#30527c",
-        status: false,
-        //check
-        openE: false,
-        openT: true,
-        show_ed: false,
+        content_ans_time_start: "19:00",
+        content_ans_time_to: " - ",
+        content_ans_time_end: "23:00",
+        //ticket type
+        content_ans_type: "一般預售票 ( Regular Ticket )",
+        //price
+        content_ans_price: "800",
+        //site
+        content_ans_site: "LINE CUBE SHIBUYA",
+        //location & map
+        content_ans_adress: "東京都渋谷区宇田川町1−1",
+        content_ans_map_url: "'https://goo.gl/maps/JnXDN6azu7jPaV9x6'",
+        //Tickets_Ticket_ticket_usingstate
+        //state
+        state_off: "活動結束 Expired",
+        state_on: "憑券入場 QR Code",
+        state: true,
       },
       {
-        //tital
-        eventtital: "SETOUCHI 2019",
-        subtital: "TRIENNALE",
+        //Tickets_Ticket_ticket_Cover_cover
+        //Ticket & Editor's openning
+        cover_infoOpenClose: false,
+        editorOpenClose: false,
+        
+        //Tickets_Ticket_ticket_Cover
         //cover image
-        imgurl: "https://miro.medium.com/max/5660/1*HoQhlB6qu_9nAx5vEhQaNw.png",
+        image_url: "'https://miro.medium.com/max/5660/1*HoQhlB6qu_9nAx5vEhQaNw.png'",
+        
+        //Tickets_Ticket_ticket_info
+        //titel 
+        name_title: "2019 瀨戶內海藝術季",
+        name_subtitle: "SETOUCHI 2019",
         //person
-        person: "fun_dream.11",
+        content_ans_id: "Funnnndream_wwww",
+        //date
+        content_ans_date_start: "2019-03-20",
+        content_ans_date_to: " - ",
+        content_ans_date_end: "2019-10-20",
         //time
-        date: "2019-03-20",
-        time_start: "10:00",
-        time_to: " - ",
-        time_end: "19:30",
-        //ticket type & price
-        type: "3 Season Ticket",
-        price: "1300",
-        //location
-        site: "Islands in Seto Inland Sea",
-        adress: "日本 岡山市と香川縣",
-        mapurl: "https://goo.gl/maps/ip52TcJXehvPeUg37",
-        //tatus
-        status_un: "UNUSED",
-        status_ued: "USED",
-        color_st: "#5E3D50",
-        status: true,
-        //check
-        openE: false,
-        openT: false,
-        show_ed: false,
+        content_ans_time_start: "10:00",
+        content_ans_time_to: " - ",
+        content_ans_time_end: "19:30",
+        //ticket type
+        content_ans_type: "三季票 ( 3 Season Ticket )",
+        //price
+        content_ans_price: "1300",
+        //site
+        content_ans_site: "瀨戶內海諸島 ( Seto Inland Sea )",
+        //location & map
+        content_ans_adress: "主航線船港 宇野港",
+        content_ans_map_url: "https://goo.gl/maps/g8pGbQ18DksUuL3k8",
+        //Tickets_Ticket_ticket_usingstate
+        //state
+        state_off: "活動結束 Expired",
+        state_on: "憑券入場 QR Code",
+        state: false,
       },
-      
     ],
     editors: [
       {
-        name: "Picture URL",
+        name: "封面圖片",
         longer: true,
         type: "url",
-        holder: "A URL of cover image.",
-        model: 'imgurl',
+        holder: "圖片連結 ( Cover Image URL)",
+        model: 'image_url',
       },
       {
-        name: "Event's Tital",
+        name: "活動名稱/主題",
         longer: false,
         type: "text",
-        holder: "Tital of the event.",
-        model: 'eventtital',
+        holder: "活動名稱 (Event/Activity Name)",
+        model: 'name_title',
       },
       {
-        name: "Subtital",
+        name: "活動副標/簡註",
         longer: false,
         type: "text",
-        holder: "The event's subtital.",
-        model: "subtital",
+        holder: "副標題 (Subtile)",
+        model: "name_subtitle",
       },
       {
-        name: "Person ID",
+        name: "持有者 ID",
         longer: false,
         type: "text",
-        holder: "Ticket's owner.",
-        model: "person",
+        holder: "用戶帳號 ( ID Name )",
+        model: "content_ans_id",
       },
       {
-        name: "Event's Date",
+        name: "活動起始日期",
         longer: false,
         type: "date",
         holder: "",
-        model: "date",
+        model: "content_ans_date_start",
       },
       {
-        name: "Start Time",
+        name: "活動結束日期",
+        longer: false,
+        type: "date",
+        holder: "",
+        model: "content_ans_date_end",
+      },
+      {
+        name: "活動開始時間",
         longer: false,
         type: "time",
         holder: "",
-        model: "time_start",
+        model: "content_ans_time_start",
       },
       {
-        name: "Endding Time",
+        name: "活動結束時間",
         longer: false,
         type: "time",
         holder: "",
-        model: "time_end",
+        model: "content_ans_time_end",
       },
       {
-        name: "Ticket Type",
+        name: "票券種類",
         longer: false,
         type: "text",
-        holder: "",
-        model: "type",
+        holder: "票種 ( Ticket Type )",
+        model: "content_ans_type",
       },
       {
-        name: "Ticket Price",
+        name: "票券售價",
         longer: false,
         type: "text",
-        holder: "",
-        model: "price",
+        holder: "票價 ( Price )",
+        model: "content_ans_price",
       },
       {
-        name: "Location",
+        name: "活動場地",
         longer: true,
         type: "text",
-        holder: "Name of the event's place.",
-        model: "site",
+        holder: "場地 ( Location )",
+        model: "content_ans_site",
       },
       {
-        name: "Adress",
+        name: "活動位址",
         longer: true,
         type: "text",
-        holder: "Adress of the event's place.",
-        model: "adress",
+        holder: "位置 ( Adress & Map )",
+        model: "content_ans_adress",
       },
       {
-        name: "Map URL",
+        name: "地圖連結",
         longer: true,
         type: "url",
-        holder: "Location on google map.",
-        model: "mapurl",
+        holder: "地圖標的連結 ( Map URL )",
+        model: "content_ans_map_url",
       },
       {
-        name: "Status Color",
-        longer: false,
-        type: "color",
-        holder: "",
-        model: "color_st",
-        
-      },
-      {
-        name: "Status",
+        name: "有效票券",
         longer: false,
         type: "checkbox",
         holder: "",
-        model: "status",
+        model: "state",
       },
-    ],
+    ]
   },
   methods: {
-    seting_close: function(id){
-      this.events[id].openE = false
+    cover_click_OpenClose: function(TKid){
+      this.events[TKid].editorOpenClose = false
     },
     
-    showEd: function(id){
-      this.events[id].show_ed = true
-    },
-    hideEd: function(id){
-      this.events[id].show_ed = false
-    },
-    
-    addticket: function(){
-      this.events.push(
-        {
-        //tital
-        eventtital: "EVENT'S TITAL",
-        subtital: "SUBTITAL",
+    AddTK: function(){
+      this.events.push({
+        //Tickets_Ticket_ticket_Cover_cover
+        //Ticket & Editor's openning
+        cover_infoOpenClose: true,
+        editorOpenClose: false,
+        
+        //Tickets_Ticket_ticket_Cover
         //cover image
-        imgurl: "",
+        image_url: " ",
+        
+        //Tickets_Ticket_ticket_info
+        //titel 
+        name_title: "活動名稱 (Event)",
+        name_subtitle: "副標題 (Subtile)",
         //person
-        person: "Owner's ID",
+        content_ans_id: "",
+        //date
+        content_ans_date: "",
         //time
-        date: "",
-        time_start: "00:00",
-        time_to: " - ",
-        time_end: "01:00",
-        //ticket type & price
-        type: "Type of the ticket",
-        price: "0",
-        //location
-        site: "Name of the event's place",
-        adress: "Adress of the event's place",
-        mapurl: "",
-        //tatus
-        status_un: "UNUSED",
-        status_ued: "USED",
-        color_st: "#30527c",
-        status: false,
-        //check
-        openE: false,
-        openT: false,
-        show_ed: false,
-      }
-      )
+        content_ans_time_start: "",
+        content_ans_time_to: " - ",
+        content_ans_time_end: "",
+        //ticket type
+        content_ans_type: "",
+        //price
+        content_ans_price: "",
+        //site
+        content_ans_site: "",
+        //location & map
+        content_ans_adress: "",
+        content_ans_map_url: "",
+        //Tickets_Ticket_ticket_usingstate
+        //state
+        state_off: "活動結束 Expired",
+        state_on: "憑券入場 QR Code",
+        state: true,
+      })
     },
-    delticket: function(id){
-      this.events.splice(id,1)
-    },
+    DeleteTK: function(TKid){
+      this.events.splice(TKid,1)
+    }
   },
 })
+
